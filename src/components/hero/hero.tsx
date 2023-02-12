cca4b5e71df7aa280830fc6d37589de92be22e8e
Samar <samarbadriddinov.tv@gmail.com>
2023-02-12 11:48:57 +0200

    review avarage

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,6 +4,7 @@ import { image_base } from 'src/helpers/constants';
 import { IMovie } from 'src/interfaces/app.interface';
 import { HeroProps } from './hero.props';
 import { TbPlayerPlay } from 'react-icons/tb';
+import ReactStars from 'react-stars';
 
 const Hero = ({ trending }: HeroProps): JSX.Element => {
 	const [movie, setMovie] = useState<IMovie>({} as IMovie);
@@ -13,8 +14,10 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
 		setMovie(randomMovie);
 	}, [trending]);
 
+	console.log(movie);
+
 	return (
-		<div className='flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end'>
+		<div className='flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:center'>
 			<div className='absolute top-0 left-0 -z-10 h-[95vh] w-full'>
 				<Image
 					src={`${image_base}${movie?.backdrop_path || movie?.poster_path}`}
@@ -24,11 +27,22 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
 				/>
 			</div>
 
+			<div className='py-[4px] px-[8px] text-center rounded-bl-[8px] rounded-tr-[8px] bg-[#1d1d1d]/50 w-[111px]'>
+				{movie.media_type}
+			</div>
+
+			<div className='flex items-center space-x-2'>
+				<ReactStars edit={false} count={10} value={movie.vote_average} color2={'#fff'} />
+				<p>({movie.vote_count})</p>
+			</div>
+
 			<h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>{movie?.title || movie?.name || movie?.original_name}</h1>
-			<p className='max-w-xs md:max-w-lg lg:max-w-2xl text-xs text-shadow-md md:text-lg lg:text-2xl'>{movie?.overview}</p>
+			<p className='max-w-xs md:max-w-lg lg:max-w-2xl text-xs text-shadow-md md:text-lg lg:text-2xl'>
+				{movie?.overview?.slice(0, 100)}...
+			</p>
 
 			<div>
-				<button className='flex justify-center items-center space-x-2 bg-white/40 font-bold text-black w-[200px] h-[56px] rounded-full'>
+				<button className='flex justify-center items-center space-x-2 bg-white/60 font-bold text-black w-[200px] h-[56px] rounded-full'>
 					<TbPlayerPlay className='h-5 w-5 md:h8 md:w-8' /> Watch now
 				</button>
 			</div>
